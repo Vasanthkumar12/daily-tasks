3f6789df20ef425ab456187fd3e73ad87ccd269c
Vasanthkumar M <vasanth23402@gmail.com>
2025-04-08 00:32:06 +0530

    Counter implemented

--- day 1/react/counter/src/App.jsx
+++ day 1/react/counter/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Counter } from './Counter'
 
 function App() {
 
   return (
     <>
-      
+      <Counter />
     </>
   )
 }
--- day 1/react/counter/src/Counter.jsx
+++ day 1/react/counter/src/Counter.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+const RESET = 'RESET'
+
+const initialState = {
+  count: 0
+}
+
+const counterReducer = (state, action) => {
+  switch(action.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + action.payload }
+
+    case DECREMENT:
+      return { ...state, count: state.count - action.payload }
+
+    case RESET:
+      return { ...state, count: 0 }
+
+    default: 
+      return state
+
+  }
+}
 
 export const Counter = () => {
-    
+    const [counter, dispatch] = useReducer(counterReducer, initialState)
+    console.log(counter)
     return (
-    <div>
-
-    </div>
+      <>
+        <div>
+          <h1>Count = {counter.count}</h1>
+          <button onClick={() => dispatch({ type: INCREMENT, payload: 1 })}>Increment</button>
+          <button onClick={() => dispatch({ type: DECREMENT, payload: 1 })}>Decrement</button>
+          <button onClick={() => dispatch({ type: RESET, payload: 1 })}>Reset</button>
+        </div>
+      </>
   )
 }
