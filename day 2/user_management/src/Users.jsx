39e86edbe14cf24198d8ff6bf12830a98668e5b2
Vasanthkumar M <vasanth23402@gmail.com>
2025-04-10 11:40:16 +0530

    Update user has done

--- day 2/user_management/src/Users.jsx
+++ day 2/user_management/src/Users.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export const Users = () => {
-    const [user, setUser] = useState({ firstName: '', lastName: '' })
+    const [user, setUser] = useState({ firstName: '', lastName: '', id: '' })
     const [users, setUsers] = useState([])
     const [filteredUsers, setFilteredUsers] = useState([])
+    const [isUpdate, setIsUpdate] = useState(false)
 
     const handleChange = (e) => {
         const {name, value} = e.target
@@ -13,8 +14,10 @@ export const Users = () => {
 
     const submitForm = async (e) => {
         e.preventDefault()
+        let data = { ...user, id: Date.now() }
+        setUser(data)
         try {
-            let res = await axios.post("https://states-c4c13-default-rtdb.firebaseio.com/users.json", user)
+            let res = await axios.post("https://states-c4c13-default-rtdb.firebaseio.com/users.json", data)
             console.log(res, 'res')
             setUser({ firstName: '', lastName: '' })
         }
@@ -26,7 +29,8 @@ export const Users = () => {
     const fetchAllUsers = async () => {
         try {
             let result = await axios.get('https://states-c4c13-default-rtdb.firebaseio.com/users.json')
-            setUsers(Object.values(result.data))
+            let usersData = result.data? Object.values(result.data) : []
+            setUsers(usersData)
         }
         catch(error) {
             console.log(error)
@@ -46,6 +50,30 @@ export const Users = () => {
         setFilteredUsers(arr)
     }
 
+    // update users
+    const updateFields = (user) => {
+        setUser(user)
+        setIsUpdate(prev => !prev)
+        console.log(user)
+    }
+
+    const updateUser = async() => {
+        
+        try {
+            let result = await axios.get('https://states-c4c13-default-rtdb.firebaseio.com/users.json')
+            let userEntry = Object.entries(result.data).find(([key, value]) => value.id == user.id)
+            console.log(userEntry, 'userEntry')
+            console.log(user)
+            let upRes = await axios.patch(`https://states-c4c13-default-rtdb.firebaseio.com/users/${userEntry[0]}.json`, user)
+            setUser({ firstName: '', lastName: '' })
+            setIsUpdate(!isUpdate)
+            upRes.status == 200 ? console.log("up Success") : console.log("upfailed")
+        }
+        catch(error) {
+            console.log(error.message)
+        }
+    }
+
   return (
     <div>
         <div>
@@ -56,9 +84,9 @@ export const Users = () => {
         <div style={{display: 'flex'}}>
             <div style={{border: '1px solid black', padding: '10px', marginBottom: "20px", marginTop: "20px" }}>
                 { filteredUsers.length > 0 ? (filteredUsers.map((user) => (
-                    <p>{user.firstName} {user.lastName}</p>
+                    <p onClick={() => updateFields(user)}>{user.firstName} {user.lastName}</p>
                 ))) : (users.map((user) => (
-                    <p>{user.firstName} {user.lastName}</p>
+                    <p onClick={() => updateFields(user)}>{user.firstName} {user.lastName}</p>
                 )))}
             </div>
 
@@ -77,9 +105,9 @@ export const Users = () => {
         </div>
 
         <div style={{ display: "flex", justifyContent: 'space-around', width: '300px' }}>
-            <button style={{padding: '5px'}} onClick={submitForm}>Create</button>
-            <button style={{padding: '5px'}}>Update</button>
-            <button style={{padding: '5px'}}>Delete</button>
+            <button style={{padding: '5px'}} onClick={submitForm} disabled={isUpdate}>Create</button>
+            <button style={{padding: '5px'}} onClick={updateUser} disabled={!isUpdate}>Update</button>
+            <button style={{padding: '5px'}} disabled={!isUpdate}>Delete</button>
             <button style={{padding: '5px'}}>Cancel</button>
         </div>
 
