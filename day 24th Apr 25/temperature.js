6ccf4790e324bef08bebb1ec8d253a6d6fa580ee
Vasanthkumar M <vasanth23402@gmail.com>
2025-04-24 12:35:41 +0530

    temperature optimization

--- day 24th Apr 25/temperature.js
+++ day 24th Apr 25/temperature.js
@@ -2,17 +2,30 @@ let T = [73, 74, 75, 71, 69, 72, 76, 73]
 let arr = new Array(T.length).fill(0)
 
 
+// for(let i=0; i<T.length; i++) {
+//     let c = 0, flag = false
+//     for(let j=i+1; j<T.length; j++) {
+//         if(T[i] < T[j]) {
+//             flag = true
+//             break
+//         }
+//         else {
+//             c++
+//         }
+//     }
+//     arr[i] = flag? c+1 : 0
+// }
+// console.log(arr)
+
+
+// Reduce TC from O(n^2) to O(n) using Stack
+
+let stk = []
 for(let i=0; i<T.length; i++) {
-    let c = 0, flag = false
-    for(let j=i+1; j<T.length; j++) {
-        if(T[i] < T[j]) {
-            flag = true
-            break
-        }
-        else {
-            c++
-        }
+    while(stk.length && T[i] > T[stk[stk.length-1]]) {
+        let pre = stk.pop()
+        arr[pre] = i - pre
     }
-    arr[i] = flag? c+1 : 0
+    stk.push(i)
 }
 console.log(arr)
