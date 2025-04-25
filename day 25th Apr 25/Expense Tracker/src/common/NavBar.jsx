9abe9ebc9ad6f1fddf03f359fcbe96dd8f1e1260
Vasanthkumar M <vasanth23402@gmail.com>
2025-04-25 22:12:06 +0530

    Applied css for Navbar is completed

--- day 25th Apr 25/Expense Tracker/src/common/NavBar.jsx
+++ day 25th Apr 25/Expense Tracker/src/common/NavBar.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import { CiHome } from "react-icons/ci";
+import { FiHome } from "react-icons/fi";
 import { GrAnalytics } from "react-icons/gr";
 import { MdHistory } from "react-icons/md";
 
 export const Navbar = () => {
     return (
-        <div>
-            <CiHome />
-            <span>Dashboard</span>
-            <GrAnalytics />
-            <span>Analytics</span>
-            <MdHistory />
-            <span>History</span>
+        <div className='flex justify-start items-center pl-10 pt-3 pb-3 shadow-md'>
+            <div className='flex justify-start items-center pl-20'>
+                <FiHome className='h-10 w-7' />
+                <span className='pl-2 text-[18px]'>Dashboard</span>
+            </div>
+            <div className='flex justify-start items-center pl-20'>
+                <GrAnalytics className='h-10 w-7' />
+                <span className='pl-2 text-[18px]'>Analytics</span>
+            </div>
+            <div className='flex justify-start items-center pl-20'>
+                <MdHistory className='h-10 w-7' />
+                <span className='pl-2 text-[18px]'>History</span>
+            </div>
         </div>
     )
 }
