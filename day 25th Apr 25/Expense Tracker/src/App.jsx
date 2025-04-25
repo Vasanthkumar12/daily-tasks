c2fbe85127edbdd6b6baa4b245f262cc1af65024
Vasanthkumar M <vasanth23402@gmail.com>
2025-04-25 22:58:20 +0530

    Applied Style for SetBudget component

--- day 25th Apr 25/Expense Tracker/src/App.jsx
+++ day 25th Apr 25/Expense Tracker/src/App.jsx
@@ -1,12 +1,13 @@
 // import './App.css'
 // import { Demo } from './Demo'
 import { Navbar } from './common/NavBar'
-
+import { SetBudget } from './components/Budget/SetBudget'
 function App() {
 
   return (
     <>
       <Navbar />
+      <SetBudget />
     </>
   )
 }
--- day 25th Apr 25/Expense Tracker/src/components/Budget/SetBudget.jsx
+++ day 25th Apr 25/Expense Tracker/src/components/Budget/SetBudget.jsx
@@ -9,22 +9,28 @@ export const SetBudget = () => {
     }
   return (
     <div>
-        <h1>Expense Tracker</h1>
-        <form onSubmit={setBudget}>
-            <input type="number" name='budget' onChange={() => setBudget(e.target.value)} placeholder='Enter budget amount' />
-            <input type="submit" value='Set Budget' />
-        </form>
-        <div>
-            <h2>Total Budget</h2>
-            <h1>$0</h1>
-        </div>
-        <div>
-            <h2>Total Expense</h2>
-            <h1>$0</h1>
-        </div>
-        <div>
-            <h2>Remaining</h2>
-            <h1>$0</h1>
+        <h1 className='text-[32px] font-bold pl-5 pt-5'>Expense Tracker</h1>
+        <div className='shadow-md m-5 p-4'>
+            <form onSubmit={setBudget} className='flex justify-between'>
+                <input type="number" name='budget' onChange={() => setBudget(e.target.value)} placeholder='Enter budget amount' 
+                    className='border p-1 pl-2 text-[18px] flex-1 mr-2 rounded-sm'
+                />
+                <input type="submit" className='border p-2 pl-5 pr-5 text-white bg-blue-600 font-bold rounded-sm' value='Set Budget' />
+            </form>
+            <div className='mt-5 flex justify-between'>
+                <div className='bg-blue-100 p-5 pr-45'>
+                    <h2 className='text-[16px]'>Total Budget</h2>
+                    <h1 className='text-[24px] font-bold'>$0</h1>
+                </div>
+                <div className='bg-green-100 p-5 pr-45'>
+                    <h2 className='text-[16px]'>Total Expense</h2>
+                    <h1 className='text-[24px] font-bold'>$0</h1>
+                </div>
+                <div className='bg-yellow-100 p-5 pr-45'>
+                    <h2 className='text-[16px]'>Remaining</h2>
+                    <h1 className='text-[24px] font-bold'>$0</h1>
+                </div>
+            </div>
         </div>
     </div>
   )
