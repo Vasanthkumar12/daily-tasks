5fa980615b662d9ebcd296a8edcc74d5acd1f3a5
Vasanthkumar M <vasanth23402@gmail.com>
2025-05-03 15:42:06 +0530

    2nd commit - pagination applied

--- day 3rd May 25/characters_api/src/components/Characters.jsx
+++ day 3rd May 25/characters_api/src/components/Characters.jsx
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react'
 export const Characters = () => {
+    const perPage = 6
     const [characters, setCharacters] = useState([])
+    const [paginatedCharacters, setPaginatedCharacters] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const [startIndex, setStartIndex] = useState(0)
+    const [endIndex, setEndIndex] = useState(perPage)
 
-    const fetchCharacters = async() => {
+    let totalPages = Math.ceil(characters.length / 6)
+
+    const fetchCharacters = async () => {
+        let newStartIndex = (currentPage - 1) * perPage
+        let newEndIndex = newStartIndex + perPage
+        setStartIndex(newStartIndex)
+        setEndIndex(newEndIndex)
+        console.log(startIndex, endIndex)
+
         try {
             let res = await fetch('https://rickandmortyapi.com/api/character')
             let data = await res.json()
             setCharacters(data.results)
+            let finalRes = data.results.slice(startIndex, endIndex)
+            setPaginatedCharacters(finalRes)
         }
         catch(error) {
             console.log(error)
@@ -15,19 +30,23 @@ export const Characters = () => {
 
     useEffect(() => {
         fetchCharacters()
-    }, [characters])
-    return (
-    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
-        { characters.map((char, index) => (
-            <ol key={index} style={{listStyleType: 'none'}}>
-                <li><img src={char.image} alt="cartoon" /></li>
-                <li>{char.name}</li>
-                <li>{char.species}</li>
-                <li>{char.status}</li>
-            </ol>
-        )) }
+    }, [currentPage])
 
-        
-    </div>
+    return (
+        <>
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+                { paginatedCharacters.map((char, index) => (
+                    <ol key={index} style={{listStyleType: 'none'}}>
+                        <li><img src={char.image} alt="cartoon" /></li>
+                        <li>{char.name}</li>
+                        <li>{char.species}</li>
+                        <li>{char.status}</li>
+                    </ol>
+                )) }
+            </div>
+            <button disabled = {currentPage <= 1? true : false} onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
+            <button>{currentPage}</button>
+            <button disabled = {currentPage >= totalPages ? true : false }  onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+        </>
   )
 }
