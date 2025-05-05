af5ad5ecff5155a6d75e8b46f26f710f2455ecae
Vasanthkumar M <vasanth23402@gmail.com>
2025-05-05 17:17:11 +0530

    Done - Part 1

--- day 5th May 25/assignment_3/src/App.jsx
+++ day 5th May 25/assignment_3/src/App.jsx
@@ -1,10 +1,40 @@
+import { useState } from 'react'
 import './App.css'
+import { Greeting } from './Greeting'
 
 function App() {
+  const [name, setName] = useState('')
+  const [showGreet, setShowGreet] = useState(false)
+
+  const timeOfDay = new Date().getHours()
+  console.log(name, timeOfDay)
+
+  const handleClick = () => {
+    if(name.trim() == '') {
+      alert('Please Enter valid name')
+      return
+    }
+    setShowGreet(true)
+  }
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
 
   return (
     <>
-      
+      <input type="text" onChange= {handleChange} value={name} placeholder='Enter your name' />
+      <button onClick={handleClick}>Greet</button>
+      { showGreet ? (<Greeting 
+        name = {name} 
+        timeOfDay = {timeOfDay}
+        showGreet = {showGreet}
+      />) 
+      : 
+      ( 
+        <h1></h1>
+      ) }
+
     </>
   )
 }
